b9333f62eb2ee45272ee4bb3c22864c5da09fcf4
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2022-01-20 21:18:11 +0100

    mix.exs fix for hex

--- mix.exs
+++ mix.exs
@@ -90,8 +90,7 @@ defmodule Estructura.MixProject do
       canonical: "http://hexdocs.pm/#{@app}",
       logo: "stuff/#{@app}-48x48.png",
       source_url: "https://github.com/am-kantox/#{@app}",
-      assets: "",
-      extras: ~w[],
+      extras: ~w[README.md],
       groups_for_modules: []
     ]
   end

83ee2864ae59b1317d33c316fc1eaff1c3f003fc
Aleksei Matiushkin <am@ambment.cat>
2025-11-06 09:59:58 +0100

    Added example for

--- examples/with_formulae/test/with_formulae_test.exs
+++ examples/with_formulae/test/with_formulae_test.exs
@@ -2,8 +2,14 @@ defmodule WithFormulaeTest do
   use ExUnit.Case
   doctest WithFormulae
 
-  test "calculated value" do
+  test "calculated value (`put_in/3`)" do
     wf = %WithFormulae{} |> put_in([:bar], ~w[a b c])
+    IO.inspect(wf)
+    assert wf.foo == 3
+  end
+
+  test "calculated value (`coerce/3`)" do
+    {:ok, wf} = Estructura.coerce(WithFormulae, %{bar: ~w[a b c]})
     IO.inspect(wf)
     assert wf.foo == 3
   end
